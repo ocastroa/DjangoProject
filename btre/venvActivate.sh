16e4747af6f0af9c2a638a70e62bb9303faed4ce
Oscar Castro <oscarcastro_777@hotmail.com>
2018-11-16 00:53:49 -0800

    Edited .gitattributes file

--- btre/venvActivate.sh
+++ btre/venvActivate.sh
@@ -1,3 +1,4 @@
 # cd \Users\MW\Documents\btre_project
 # ./venv/Scripts/activate
 # python manage.py sqlmigrate <appname> <migration num i.e. 0001> to view sql statements for migrations
+# git push -f repoName
